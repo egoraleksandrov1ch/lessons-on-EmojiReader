b43a5fcb77ecf722e22de2657c35c80daf0cbcec
Egor Aleksandrovich <egor09662@gmail.com>
2022-12-11 16:04:27 +0100

    1
    1

---
+++ EmojiReader/EmojiModel.swift
@@ -0,0 +1,15 @@
+//
+//  EmojiModel.swift
+//  EmojiReader
+//
+//  Created by egor aleksandrovich on 11.12.2022.
+//
+
+import Foundation
+
+struct Emoji {
+    var emoji: String
+    var name: String
+    var discription: String
+    var isFavourite: Bool
+}
---
+++ EmojiReader/EmojiTableViewController.swift
@@ -0,0 +1,73 @@
+//
+//  EmojiTableViewController.swift
+//  EmojiReader
+//
+//  Created by egor aleksandrovich on 11.12.2022.
+//
+
+import UIKit
+
+class EmojiTableViewController: UITableViewController {
+    
+    var objects = [
+        Emoji(emoji: "🥰", name: "Love", discription: "Let's love each other", isFavourite: false),
+        Emoji(emoji: "🏀", name: "Football", discription: "Let's play football together", isFavourite: false),
+        Emoji(emoji: "🐱", name: "Cat", discription: "Cat is the cutest animal", isFavourite: false)
+    ]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        self.title = "Emoji Reader"
+        self.navigationItem.leftBarButtonItem = self.editButtonItem
+        
+        
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return objects.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "emojiCell", for: indexPath) as! EmojiTableViewCell
+        let object = objects[indexPath.row]
+        cell.set(object: object)
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            objects.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        let movedEmoji = objects.remove(at: sourceIndexPath.row)
+        objects.insert(movedEmoji, at: destinationIndexPath.row)
+        tableView.reloadData()
+    }
+   
+
+}
