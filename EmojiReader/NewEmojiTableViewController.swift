068dae4a0d8b7b36d7ea46d151f0a1f797a41374
Egor Aleksandrovich <egor09662@gmail.com>
2022-12-11 17:37:34 +0100

    end1

--- EmojiReader/NewEmojiTableViewController.swift
+++ EmojiReader/NewEmojiTableViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class NewEmojiTableViewController: UITableViewController {
     
+    var emoji = Emoji(emoji: "", name: "", discription: "", isFavourite: false)
+    
     @IBOutlet weak var emojiTextField: UITextField!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var descriptionTextField: UITextField!
@@ -16,9 +18,36 @@ class NewEmojiTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        updateSaveButtonState()
+        updateUI()
+    }
+    
+    private func updateSaveButtonState() {
+        let emojiText = emojiTextField.text ?? ""
+        let nameText = nameTextField.text ?? ""
+        let descriptionText = descriptionTextField.text ?? ""
+        
+        saveButton.isEnabled = !emojiText.isEmpty && !nameText.isEmpty && !descriptionText.isEmpty
     }
 
+    private func updateUI() {
+        emojiTextField.text = emoji.emoji
+        nameTextField.text = emoji.name
+        descriptionTextField.text = emoji.discription
+    }
+    
     @IBAction func textChanged(_ sender: UITextField) {
+        updateSaveButtonState()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        super.prepare(for: segue, sender: sender)
+        guard segue.identifier == "saveSegue" else { return }
+        let emoji = emojiTextField.text ?? ""
+        let name = nameTextField.text ?? ""
+        let discription = descriptionTextField.text ?? ""
+        
+        self.emoji = Emoji(emoji: emoji, name: name, discription: discription, isFavourite: self.emoji.isFavourite)
     }
 }
